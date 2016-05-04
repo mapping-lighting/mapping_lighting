1348a5346eb3a66ddab04d5ca4b13e4bf5149fd7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-05-05 09:16:12 +1000

    Move Lighting Type to Observation, Add jitter to seeds

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,10 @@
 class Observation < ActiveRecord::Base
   # serialize :image_exif
   mount_uploader :attachment, ImageUploader
+
+  AVAILABLE_LIGHTING_TYPES = [['Street lamp', 'street'], ['Bus stop','bus'], ['Retail/Industry', 'retail_industry'], ['Car park', 'car_park'], ['Advertising - General', 'advertising_general'], ['Other', 'other']]
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,18 @@ User.create(email: 'garrow.bedrossian@gmail.com', password: 'mapping')
 
 # Open image files, and create uploads
 
+def jitter(number)
+  jitter_amount = rand() / 8
+  op = [:+, :-].sample
+  number.send(op, jitter_amount)
+end
+Observation::AVAILABLE_LIGHTING_TYPES.sample.last
+
+
 Observation.create(
   attachment: open('app/assets/images/IMG_3955.JPG'),
-  recorder_location_data: {"latitude"=>-37.819766, "longitude"=>145.039041, "accuracy"=>40 },
-  lighting_type: 'street',
+  recorder_location_data: {"latitude"=> jitter(-37.819766), "longitude"=>jitter(145.039041), "accuracy"=>40 },
+  lighting_type: Observation::AVAILABLE_LIGHTING_TYPES.sample.last,
   note: 'This is a note',
   email: 'user_1@example.com',
   name: 'User 1',
@@ -20,18 +28,18 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3956.JPG'),
-  recorder_location_data: {"latitude"=>-37.820153, "longitude"=>145.041079, "accuracy"=>40 },
-  lighting_type: 'street',
+  recorder_location_data: {"latitude"=>jitter(-37.820153), "longitude"=>jitter(145.041079), "accuracy"=>40 },
+  lighting_type: Observation::AVAILABLE_LIGHTING_TYPES.sample.last,
   note: 'This is another note',
   email: 'user_1@example.com',
   name: 'User 1',
   permission_given: true,
 )
 
-Observation.create(
+Observation.create!(
   attachment: open('app/assets/images/IMG_3957.JPG'),
-  recorder_location_data: {"latitude"=>-37.819094, "longitude"=>145.038990, "accuracy"=>40 },
-  lighting_type: 'bus',
+  recorder_location_data: {"latitude"=>jitter(-37.819094), "longitude"=> jitter(145.038990), "accuracy"=>40 },
+  lighting_type: Observation::AVAILABLE_LIGHTING_TYPES.sample.last,
   note: 'This is a note from User 2',
   email: 'user_2@example.com',
   name: 'User 2',
@@ -40,8 +48,8 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3958.JPG'),
-  recorder_location_data: {"latitude"=>-37.823047, "longitude"=>145.038861, "accuracy"=>40 },
-  lighting_type: 'street',
+  recorder_location_data: {"latitude"=> jitter(-37.823047), "longitude"=> jitter(145.038861), "accuracy"=>40 },
+  lighting_type: Observation::AVAILABLE_LIGHTING_TYPES.sample.last,
   note: 'This is a note from User 3',
   email: 'user_3@example.com',
   name: 'User 3',
@@ -50,7 +58,7 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3959.JPG'),
-  recorder_location_data: {"latitude"=>-37.821804, "longitude"=>145.041982, "accuracy"=>40 },
+  recorder_location_data: {"latitude"=> jitter(-37.821804), "longitude"=> jitter(145.041982), "accuracy"=>40 },
   lighting_type: 'car_park',
   note: 'This is another note from User 2',
   email: 'user_2@example.com',
@@ -60,7 +68,7 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3960.JPG'),
-  recorder_location_data: {"latitude"=>-37.822483, "longitude"=>145.035235, "accuracy"=>40 },
+  recorder_location_data: {"latitude"=> jitter(-37.822483), "longitude"=> jitter(145.035235), "accuracy"=>40 },
   lighting_type: 'car_park',
   note: 'This is another note from User 2',
   email: 'user_2@example.com',
@@ -70,7 +78,7 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3961.JPG'),
-  recorder_location_data: {"latitude"=>-37.822319, "longitude"=>145.039882, "accuracy"=>40 },
+  recorder_location_data: {"latitude"=> jitter(-37.822319), "longitude"=> jitter(145.039882), "accuracy"=>40 },
   lighting_type: 'car_park',
   note: 'This is another note from User 2',
   email: 'user_2@example.com',
@@ -80,7 +88,7 @@ Observation.create(
 
 Observation.create(
   attachment: open('app/assets/images/IMG_3962.JPG'),
-  recorder_location_data: {"latitude"=>-37.822394, "longitude"=>145.034838, "accuracy"=>40 },
+  recorder_location_data: {"latitude"=> jitter(-37.822394), "longitude"=> jitter(145.034838), "accuracy"=>40 },
   lighting_type: 'car_park',
   note: 'This is another note from User 2',
   email: 'user_4@example.com',
