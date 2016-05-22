8b7daacf39359c011b4d1a5676980cf9f35cd32c
Keira Hodgkison <keira@cultureamp.com>
2016-05-23 03:14:39 +1000

    Add confirmations controller and confirmations migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # new function to set the password without knowing the current
   # password used in our confirmation controller.
