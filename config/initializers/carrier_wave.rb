3225f43a98dc7657e2c16594cc87b7203100cda5
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-05-22 15:09:56 +1000

    Dont upload on CI

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,29 +2,26 @@ require 'carrierwave/orm/activerecord'
 
 # Don't attempt to upload to S3 on CI
 if ENV['CI']
-
-  Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
   end
-
 else
 
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-      region:                'ap-southeast-2',                  # optional, defaults to 'us-east-1'
-      # host:                  's3.example.com',             # optional, defaults to nil
-      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  # Production is '2016-mapping-lighting' You can configure this in your .env file locally.
-  config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+        provider:              'AWS',                        # required
+        aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+        region:                'ap-southeast-2',                  # optional, defaults to 'us-east-1'
+        # host:                  's3.example.com',             # optional, defaults to nil
+        # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    # Production is '2016-mapping-lighting' You can configure this in your .env file locally.
+    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
 
-  config.fog_public     = true                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-end
+    config.fog_public     = true                                        # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
 end
